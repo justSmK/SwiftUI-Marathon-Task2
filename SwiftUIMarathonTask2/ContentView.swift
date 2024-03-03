c4fefde8d91c3b497be11666044b701284728763
justSmK <28645140+justSmK@users.noreply.github.com>
2024-03-03 12:33:15 +0300

    Solution

--- SwiftUIMarathonTask2/ContentView.swift
+++ SwiftUIMarathonTask2/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sliderValue: Double = 300
+    private let sliderRange: ClosedRange<Double> = 100...300
+    private let sliderStep: Double = 1
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextContainer()
+                .frame(
+                    width: sliderValue,
+                    height: 250
+                )
+                .border(.red)
+                .animation(
+                    .easeInOut(duration: 0.5),
+                    value: sliderValue
+                )
+            Slider(
+                value: $sliderValue,
+                in: sliderRange,
+                step: sliderStep
+            )
         }
         .padding()
+    }
+}
+
+struct TextContainer: View {
+    var body: some View {
+        Text("Marathon ")
+            .foregroundStyle(.gray)
+            .font(.body)
+        +
+        Text("по SwiftUI ")
+            .foregroundStyle(.black)
+            .font(.headline)
+        +
+        Text("\"Отцовский пинок\"")
+            .foregroundStyle(.blue)
+            .font(.largeTitle)
     }
 }
 
